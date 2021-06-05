ec48da625fd14f900160f8f8d800bcfaddae53f4
mkaraca25 <61596919+mkaraca25@users.noreply.github.com>
2021-06-05 12:39:28 +0300

    Day9HW1,2 Db update edildi. tüm isterler yerine getirildi.
    05.06.2021

--- hrms/hrms/src/main/java/kariyernet/hrms/business/abstracts/JobSeekerCVSkillService.java
+++
@@ -1,9 +0,0 @@
-package kariyernet.hrms.business.abstracts;
-import kariyernet.hrms.core.adapters.BaseService;
-import kariyernet.hrms.core.utilities.result.DataResult;
-import kariyernet.hrms.entities.concretes.*;
-import java.util.List;
-public interface JobSeekerCVSkillService extends BaseService<JobSeekerCVSkill, Integer> {
-	DataResult<List<JobSeekerCVSkill>> getAllByJobSeekerCV_Id(int jobSeekerCVId);
-
-}
--- hrms/hrms/src/main/java/kariyernet/hrms/dataAccess/abstracts/JobSeekerCVImageDao.java
+++
@@ -1,9 +0,0 @@
-package kariyernet.hrms.dataAccess.abstracts;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kariyernet.hrms.entities.concretes.JobSeekerCVImage;
-public interface JobSeekerCVImageDao extends JpaRepository<JobSeekerCVImage, Integer> {
-		List<JobSeekerCVImage> findAllByJobSeekerCV_Id(int jobSeekerCVId);
-}
